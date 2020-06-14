3ca22a5e0c24fa0a0576e2fde1d8ce1b84a87e8e
Daniel Pieczyński <pieczynski.daniel@gmail.com>
2020-06-14 22:29:52 +0200

    Properties handling added

--- src/com/notepad/MainView.java
+++ src/com/notepad/MainView.java
@@ -1,7 +1,11 @@
 package com.notepad;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class MainView {
     private JPanel mainPanel;
@@ -28,14 +32,33 @@ public class MainView {
         newMenuButton.addActionListener(newFileActionListener);
         saveAsMenuButton.addActionListener(saveAsFileActionListener);
         aboutMenuButton.addActionListener(aboutActionListener);
+
     }
 
     public static void main(String[] args) {
+        PropertiesHandler propertiesHandler = new PropertiesHandler();
+
         JFrame frame = new JFrame("Notepad");
         frame.setContentPane(new MainView().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setSize(propertiesHandler.getWindowWidth(), propertiesHandler.getWindowHeight());
+        frame.setLocation(propertiesHandler.getWindowPositionX(), propertiesHandler.getWindowPositionY());
         frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                propertiesHandler.setWindowHeight(frame.getHeight());
+                propertiesHandler.setWindowWidth(frame.getWidth());
+                propertiesHandler.setWindowPositionX(frame.getLocation().x);
+                propertiesHandler.setWindowPositionY(frame.getLocation().y);
+                propertiesHandler.saveProperties();
+                System.out.println("Test");
+                super.windowClosing(e);
+            }
+        });
+
+
     }
 
 }
